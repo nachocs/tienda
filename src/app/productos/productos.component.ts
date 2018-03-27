7f453071e25c55f0a687bb8687a4cbdf44bb1f19
Nacho Carmona <pedro.carmona@extern.isban.com>
2018-03-27 18:07:50 -0400

    basics

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import { Service } from '../service/service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-productos',
@@ -10,15 +11,32 @@ import { Service } from '../service/service';
 })
 export class ProductosComponent implements OnInit {
   producto: any;
+  loading: Boolean = false;
+  root_url: String = 'http://www1.dreamers.com';
   constructor(private route: ActivatedRoute, private router: Router, private service: Service) { }
   ngOnInit() {
+    this.loading = true;
     this.route.paramMap
       .subscribe((params: ParamMap) => {
         this.service.getProduct(params.get('id')).subscribe((res) => {
-          this.producto = res;
+          this.loading = false;
+          this.producto = this.serializer(res);
+
           console.log(this.producto);
         });
       });
   }
-
+  serializer(model) {
+    let time;
+    if (model.TIME_A) {
+      time = new Date();
+      time.setTime(model.TIME_A + '000');
+      model.date = moment(time).format('l');
+    } else if (model.FECHA) {
+      time = new Date();
+      time.setTime(model.FECHA + '000');
+      model.date = moment(time).format('l');
+    }
+    return model;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'hammerjs';
+import fontawesome from '@fortawesome/fontawesome';
+import * as faShoppingCart from '@fortawesome/fontawesome-free-solid/faShoppingCart';
+fontawesome.library.add(faShoppingCart);
 
 if (environment.production) {
   enableProdMode();
